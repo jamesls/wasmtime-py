d589a5c357baebf28f79a19fd3a67561666ff193
James Saryerwinnie <js@jamesls.com>
2024-04-18 19:17:12 -0400

    REMOVEME: Allow the bootstrap cmd to accept an optional name
    Makes it easier to use for testing with other components.

--- rust/src/bin/bootstrap.rs
+++ rust/src/bin/bootstrap.rs
@@ -4,11 +4,12 @@ fn main() {
     let mut args = std::env::args().skip(1);
     let component = args.next().unwrap();
     let out_dir = PathBuf::from(args.next().unwrap());
+    let binding_name = args.next().unwrap_or_else(|| "bindgen".to_string());
 
     let mut gen = bindgen::WasmtimePy::default();
     let mut files = Default::default();
     let component = std::fs::read(&component).unwrap();
-    gen.generate("bindgen", &component, &mut files).unwrap();
+    gen.generate(&binding_name, &component, &mut files).unwrap();
 
     for (name, bytes) in files.iter() {
         let path = out_dir.join(name);
